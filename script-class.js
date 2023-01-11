1b9b48ca2f3a53dbcb875fa054b05b6c8561ab43
abahuugbede_dev <89143961+EngruugbeJimmy@users.noreply.github.com>
2023-01-11 16:19:46 +0100

    booksdisplay

--- script-class.js
+++ script-class.js
@@ -35,3 +35,58 @@ class Library {
   }
 }
 
+function Display() {
+  let getBooks = localStorage.getItem('books');
+  getBooks = JSON.parse(getBooks);
+  if (getBooks != null) {
+    const booksSection = document.getElementById('books');
+    let book = '';
+    for (let i = 0; i < getBooks.length; i += 1) {
+      let background;
+      if (i % 2 === 0) {
+        background = 'gray';
+      } else {
+        background = 'white';
+      }
+      book += `<div class="book ${background}"><p>${getBooks[i].title} by ${getBooks[i].author}</p>
+      <button class="button" value=${i}>Remove</button>
+      </div>`;
+    }
+    booksSection.innerHTML = `${book}`;
+  }
+}
+Display();
+function GetContent() {
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
+  const titleBook = title.value;
+  const authorBook = author.value;
+  const message = document.querySelector('#message');
+  if (titleBook === '' || authorBook === '') {
+    message.classList.remove('show');
+    return;
+  }
+  const book = new Book(titleBook, authorBook);
+  let storage = localStorage.getItem('books');
+  storage = JSON.parse(storage);
+  const library = new Library(book, storage);
+  library.add();
+  Display();
+  // eslint-disable-next-line no-restricted-globals
+  location.reload();
+}
+buttonAdd.addEventListener('click', GetContent);
+const deleteBtn = document.querySelectorAll('.button');
+function GetIndex() {
+  const library = new Library(null, null, index);
+  library.remove();
+  Display();
+  // eslint-disable-next-line no-restricted-globals
+  location.reload();
+}
+deleteBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    index = btn.value;
+    GetIndex();
+  });
+});
